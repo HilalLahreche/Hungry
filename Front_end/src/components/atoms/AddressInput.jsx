6715ae945d3274a66a53f998bfa61631b7839145
Z0uLf4T <A.Zoulfat@gmail.com>
2024-09-16 17:45:32 +0200

    address input colors updated

--- Front_end/src/components/atoms/AddressInput.jsx
+++ Front_end/src/components/atoms/AddressInput.jsx
@@ -1,10 +1,10 @@
 
 const AddressInput = () => {
   return <>
-            <div className="mt-6 flex max-w-md gap-x-2 ">
+            <div className="m-6 flex max-w-md gap-x-1">
               <label htmlFor="user-address" className="sr-only"></label>
-              <input id="user-address" name="user-address" type="text" required className="min-w-0 flex-auto rounded-full border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Enter your address"/>
-              <button type="submit" className="flex-none rounded-full bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+              <input id="user-address" name="user-address" type="text" required className="min-w-0 flex-auto rounded-full border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6" placeholder="Enter your address"/>
+              <button type="submit" className="flex-none rounded-full bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">
               <svg className="h-[1.3em] w-[1.3em] fill-[#b4b4b4]" aria-hidden="true" viewBox="0 0 24 24">
             <g>
                 <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
